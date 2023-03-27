6f77ea62f8e40eaeeddc49296e861ec46eafb284
Danil Shendel <124462567+shendanil@users.noreply.github.com>
2023-03-27 23:02:16 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 import CommentSection from './assets/components/CommentSection'
-import CommentSectionTest from './assets/components/CommentSectionTest'
 
 import Attribution from './assets/components/Attribution'
 import './index.css'
@@ -9,8 +8,7 @@ import './index.css'
 function App() {
   return (
     <div className='scroll-smooth bg-gradient-to-tr from-indigo-200 to-violet-100 h-full p-12 sm:p-4'>
-      {/* <CommentSection /> */}
-      <CommentSectionTest />
+      <CommentSection />
       <Attribution />
     </div>
   )
